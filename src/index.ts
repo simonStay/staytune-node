4575c62ad467f25ac4daaac45490753d08f2ed0a
rav <atxwebdesigner2@gmail.com>
2019-12-24 14:52:44 +0530

    added minute changes to login

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ const cron = require('node-cron');
 import {UserController} from './controllers/user.controller';
 import axios from 'axios';
 
-// cron.schedule('* 13 * * *', async () => {
-//   console.log('running a task every 2 minutes');
-//   const result = await axios.get(
-//     'https://staytune.austinconversionoptimization.com/users/push-notifications',
-//   );
-//   console.log('cron result : ', result);
-// });
+cron.schedule('* 13 * * *', async () => {
+  console.log('running a task every 2 minutes');
+  const result = await axios.get(
+    'https://staytune.austinconversionoptimization.com/users/push-notifications',
+  );
+  console.log('cron result : ', result);
+});
 
 export {StayTuneApplication};
 
